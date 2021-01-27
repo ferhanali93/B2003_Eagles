4cc4105114214c713f95534f9ab606d11b1c9e01
ferhanali <Bronx_10460>
2021-01-27 00:03:17 -0500

    git commit -m "added Loopy.java"

---
+++ src/Loopy.java
@@ -0,0 +1,15 @@
+public class Loopy {
+    public static void main(String[] args) {
+       int x = 2;
+        System.out.println("Before the Loop, the value of x is: " + x);
+
+        while (x < 100 ) {
+            System.out.println("In the Loop");
+            System.out.println("value of x is " + x);
+            x = x + 1;
+
+        }
+
+
+    }
+}
